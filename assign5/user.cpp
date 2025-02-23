fbb6fee0df68ad26bf1cb9c6233502da3dd19bde
Zhengqiang Liao <lzq666amn@gmail.com>
2025-02-23 14:01:31 +0800

    Define ......50%

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -63,3 +63,73 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+std::ostream& 
+operator<<(std::ostream& os, const User& u) {
+  os << "User(name=" << u._name << ", friends=[";
+
+  for (int i = 0; i < u.size(); ++i) {
+    os << u._friends[i];
+    if (i < u.size() - 1) {
+      os << ", ";
+    }
+  }
+
+  os << "])";
+  return os;
+}
+
+// 
+User::~User() {
+  delete[] _friends;
+}
+
+// 
+User::User(const User& user) : _size(user._size) {
+  _friends = new std::string[_size];
+  for (int i = 0; i < _size; i++) {
+    _friends[i] = user._friends[i];
+  }
+}
+
+//
+User& User::operator=(const User& user) {
+  if (this != &user) {
+    _name = user._name;
+    _friends = user._friends;
+    _size = user._size;
+  }
+  return *this;
+}
+
+// 
+User::User(User&& user) noexcept : 
+_name(user._name), _friends(user._friends), 
+_size(user._size), _capacity(user._capacity)
+{
+  _name = 
+  _friends = 
+  _size = 
+}
+
+// 
+User& User::operator=(User&& user) noexcept
+{
+  if (this != &user) {
+    delete[] _friends;
+    _name = user._name;
+    _friends = user._friends;
+    _size = user._size;
+    _capacity = user._capacity;
+    user._friends = nullptr;
+    user._size = 0;
+  }
+  return *this;
+}
+
+User& User::operator+=(const User& user) {
+
+}
+
+User& User::operator<(const User& user) const {
+
+}
--- assign5/user.h
+++ assign5/user.h
@@ -14,11 +14,19 @@ public:
   std::string get_name() const;
   size_t size() const;
   void set_friend(size_t index, const std::string& name);
+  friend std::ostream& operator<<(std::ostream& os, const User& u);
 
   /** 
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  ~User();
+  User(const User& user);
+  User& User::operator=(const User& user);
+  User::User(User&& user) noexcept;
+  User& User::operator=(User&& user) noexcept;
+  User& User::operator+=(const User& user);
+  bool User::operator<(const User& user) const;
 
 private:
   std::string _name;
